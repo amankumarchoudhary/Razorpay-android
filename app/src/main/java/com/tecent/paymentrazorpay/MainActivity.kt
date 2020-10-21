3e88f39c3638a2b442eaa8ec0ff4b84f9ef90d36
amanchoudharycse <choudhary625kumar@gmail.com>
2020-10-21 15:27:18 +0530

    first commit

---
+++ app/src/main/java/com/tecent/paymentrazorpay/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.tecent.paymentrazorpay
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.razorpay.Checkout
+import com.razorpay.PaymentResultListener
+import com.tecent.paymentrazorpay.databinding.ActivityMainBinding
+import org.json.JSONObject
+
+
+class MainActivity : AppCompatActivity(), PaymentResultListener {
+    val TAG = "gateway"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        /**
+         * Preload payment resources
+         */
+        Checkout.preload(applicationContext)
+
+        binding.btnPay.setOnClickListener {
+            startPayment()
+        }
+    }
+
+    fun startPayment() {
+        /**
+         * Instantiate Checkout
+         */
+        val checkout = Checkout()
+//        checkout.setKeyID("rzp_test_FIyDsosGNRebtc")
+        /**
+         * Set your logo here
+         */
+//        checkout.setImage(R.drawable.logo)
+        /**
+         * Reference to current activity
+         */
+        val activity: Activity = this
+        /**
+         * Pass your payment options to the Razorpay Checkout as a JSONObject
+         */
+        try {
+            val options = JSONObject()
+            options.put("name", "Tecent")
+            options.put("description", "Test order")
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png")
+//            options.put("order_id", "order_DBJOWzybf0sJbb") //from response of step 3.
+            options.put("theme.color", "#3399cc")
+            options.put("currency", "INR")
+            options.put("amount", "100") //pass amount in currency subunits
+            options.put("prefill.email", "aman.kumar.choudhary@yopmail.com")
+            options.put("prefill.contact", "1234567898")
+            checkout.open(activity, options)
+        } catch (e: Exception) {
+            Log.e(TAG, "Error in starting Razorpay Checkout", e)
+        }
+    }
+
+    override fun onPaymentError(p0: Int, p1: String?) {
+        Toast.makeText(this, "Payment failed", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPaymentSuccess(p0: String?) {
+        Toast.makeText(this, "Payment Successful", Toast.LENGTH_SHORT).show()
+    }
+}
